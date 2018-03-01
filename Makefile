ae6738b107cfd3182602931e5e4b460f9b15737c
Frederic Stark <frederic.stark@gmail.com>
2018-03-01 19:45:06 +0100

    Reading files

--- Makefile
+++ Makefile
@@ -2,4 +2,7 @@ hc: hc.o
 	c++ hc.o -o hc
 
 hc.o:	hc.cpp
-	c++ -c -O3 hc.cpp -o hc.o
+	c++ -c -g hc.cpp -o hc.o
+
+clean:
+	rm hc hc.o
--- hc.cpp
+++ hc.cpp
@@ -1,19 +1,46 @@
 #include <iostream>
 #include <fstream>
 #include <assert.h>
+#include <vector>
 
 using namespace std;
 
-int w;
-int h;
-
-
 //  A car in the simulation
 
-struct car
+struct xyt
 {
-
+    int x;
+    int y;
+    int t;
 };
+
+struct ride
+{
+    xyt from;
+    xyt to;
+};
+
+struct pb
+{
+    int width;
+    int height;
+    int car_count;
+    int ride_count;
+    int bonus;
+    int max_time;
+
+    std::vector<ride> rides;
+};
+
+void read( istream &i, ride &r )
+{
+    i >> r.from.y;
+    i >> r.from.x;
+    i >> r.to.y;
+    i >> r.to.x;
+    i >> r.from.t;
+    i >> r.to.t;
+}
 
 int main( int argc, char **argv )
 {
@@ -21,9 +48,22 @@ int main( int argc, char **argv )
 
     ifstream pbfile;
     pbfile.open( argv[1] );
-    int n;
-    pbfile >> n;
-    std::cout << n << std::endl;
+
+    pb p;
+    pbfile >> p.height;
+    pbfile >> p.width;
+    pbfile >> p.car_count;
+    pbfile >> p.ride_count;
+    pbfile >> p.bonus;
+    pbfile >> p.max_time;
+    
+    for (auto i=0;i!=p.ride_count;i++)
+    {
+        ride r;
+        read( pbfile, r );
+        p.rides.push_back( r );
+    }
+
     pbfile.close();
     return 0;
 }
