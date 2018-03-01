8a5680a5190ac8086b28303505d1a98397b3b002
Frederic Stark <frederic.stark@gmail.com>
2018-03-01 21:20:04 +0100

    Fix

--- doit.sh
+++ doit.sh
@@ -1,5 +1,7 @@
-# ./hc data/a.in > out/b.out
+make
+
+./hc data/a.in > out/a.out
 ./hc data/b.in > out/b.out
-./hc data/c.in > out/b.out
-./hc data/d.in > out/b.out
-./hc data/e.in > out/b.out
+./hc data/c.in > out/c.out
+./hc data/d.in > out/d.out
+./hc data/e.in > out/e.out
